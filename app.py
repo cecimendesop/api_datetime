8e27bca9e8d1986750c9d14ab66b495005a3b0b8
cecimendesop <cecilia.mopaulo@gmail.com>
2025-03-27 08:44:38 -0300

    sfghdsfh

--- app.py
+++ app.py
@@ -4,8 +4,32 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+
+
 @app.route('/verificar-data/<data>', methods=['GET'])
 def verificar_data(data):
+    """
+    Verifica a data para definir o período de tempo a qual ela pertence (passado, presente, futuro)
+
+    #Endpoint:
+    'GET /verificar-data/<data>'
+
+    :param data: data indicada
+    :return: período de tempo a qual ela pertence (passado, presente ou futuro)
+
+    #Resposta (JSON):
+    {
+            "situacao": status,
+            "diferenca": {
+                "dias": diferenca_dias,
+                "meses": diferenca_meses,
+                "anos": diferenca_anos
+            }
+
+    #Erros Possíveis:
+    {"erro": "Formato de data inválido. Use YYYY-MM-DD."}
+
+    """
     try:
         data_informada = datetime.strptime(data, "%Y-%m-%d").date()
         data_atual = datetime.now().date()
